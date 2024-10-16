8315fcbd613fcd7f29394b9ad04517721ef430f8
murilo.vcaetano <murilo.vcaetano@ITQ0675982W10-1>
2024-10-16 17:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ SysTINSApp/FrmPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysTINSApp
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void produtoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fechamentoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void novoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            FrmUsuarios frmUsuarios = new();
+            frmUsuarios.MdiParent = this;//isso faz com que deixe ele dentro do conteiner na parte cinza e não abra duas telas a partir do momento em que você clicar no icone ou botão
+            frmUsuarios.Show();//Se coloca show Dialog ele abre duas telinhas quando clicar no botão mas tem um porem ele so fecha se voce terminar o processo
+        }
+    }
+}
